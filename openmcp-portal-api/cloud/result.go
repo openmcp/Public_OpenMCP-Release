7aacb46bcebb34201c15e21b195cb3af75df1695
khlee-gmd <67941834+khlee-gmd@users.noreply.github.com>
2022-02-14 15:39:38 +0900

    Delete openmcp-portal-api directory

--- openmcp-portal-api/cloud/result.go
+++
@@ -1,9 +0,0 @@
-package cloud
-
-type AddNodeResult struct {
-	Result     string `json:"result"`
-	InstanceID string `json:"instanceid"`
-	ErrMessage string `json:"errMessage"`
-}
-
-type AddNodeResults []AddNodeResult
