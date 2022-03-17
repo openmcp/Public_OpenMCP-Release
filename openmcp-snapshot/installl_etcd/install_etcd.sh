a1f1ee58d1baa15e8104c3dcd491e45d0e017440
gentian0185 <gentian0185@gmail.com>
2022-03-17 14:37:01 +0900

    Update install_etcd.sh

--- openmcp-snapshot/installl_etcd/install_etcd.sh
+++ openmcp-snapshot/installl_etcd/install_etcd.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+## 현재 경로에 etcd-3.4.3를 만들어 설치하는 스크립트. ${PWD}/etcd-3.4.3 경로에 설치 폴더 생성
+INSTALL_FOLDER=`pwd`/etcd-3.4.3
+
 HOST_IP=${HOST_IP}   # input target server IP (ex.10.0.0.226)
 HOST_NAME=${HOST_NAME}  # input target Server Hostname (ex.nanmdev6)
 ETCD_VER=v3.4.3   #kube 1.17에서 사용하는 버전
@@ -16,8 +19,8 @@ curl -L ${DOWNLOAD_URL}/${ETCD_VER}/etcd-${ETCD_VER}-linux-amd64.tar.gz -o /tmp/
 tar xzvf /tmp/etcd-${ETCD_VER}-linux-amd64.tar.gz -C /tmp/etcd-download-test --strip-components=1
 rm -f /tmp/etcd-${ETCD_VER}-linux-amd64.tar.gz
 
-mv /tmp/etcd-download-test ./etcd-3.4.3
-cd ./etcd-3.4.3
+mv /tmp/etcd-download-test ${INSTALL_FOLDER}
+cd  ${INSTALL_FOLDER}
 
 echo "install success"
 
